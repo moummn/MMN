e0da328058ae2832b15c527af571c5cf8b6aad2b
某M <mmnstar@hotmail.com>
2016-07-28 12:18:20 +0800

    160728

---
+++ getPIDKey/getPIDKey/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(634, 150)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(0, 181)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(634, 181)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(1, 152)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(634, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = ">>"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.Button1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(635, 482)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "GetPIDKey"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ getPIDKey/getPIDKey/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Function fnGETPID(ByVal startPoint As Integer, ByVal StrString As String, ByRef PIDString As String) As Integer
+        Dim I As Integer = InStr(startPoint, StrString, "-")
+        If I = 0 OrElse I < 6 OrElse I > Len(StrString) - 23 Then
+            Return 0
+            Exit Function
+        End If
+        If Mid(StrString, I + 6, 1) = "-" AndAlso Mid(StrString, I + 12, 1) = "-" AndAlso Mid(StrString, I + 18, 1) = "-" Then
+            PIDString = Mid(StrString, I - 5, 29)
+            Return I + 24
+            Exit Function
+        End If
+        Return 0
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Call Form1_Resize(sender, e)
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        TextBox1.Height = (Me.ClientSize.Height - Button1.Height) / 2
+        TextBox2.Height = TextBox1.Height
+        TextBox2.Top = (Me.ClientSize.Height + Button1.Height) / 2
+        Button1.Top = TextBox1.Height
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim S As String = TextBox1.Text
+        Dim PID As String = ""
+        Dim TEXT As String = ""
+        Dim I As Integer = 1
+        Do
+            I = fnGETPID(I, S, PID)
+            If I = 0 Then Exit Do
+            TEXT = TEXT & PID & vbCrLf
+        Loop
+        TextBox2.Text = TEXT
+    End Sub
+End Class
