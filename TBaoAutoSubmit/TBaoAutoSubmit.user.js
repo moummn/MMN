b26a2dfeb597c91121369a0221bbde0c54068ae3
某M <mmnstar@hotmail.com>
2016-12-09 12:50:12 +0800

    20161209

--- TBaoAutoSubmit/TBaoAutoSubmit.user.js
+++ TBaoAutoSubmit/TBaoAutoSubmit.user.js
@@ -11,6 +11,7 @@
 // @updateURL   https://github.com/moummn/MMN/raw/master/TBaoAutoSubmit/TBaoAutoSubmit.user.js
 // ==/UserScript==
 
+var t = setTimeout("location.reload()", 1000);
 var SubmitArea = document.getElementById("submitOrder_1");
 var SubmitElem = SubmitArea.getElementsByTagName("a")[1];
 //window.alert(SubmitElem.innerHTML);
