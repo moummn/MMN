39dc4a98083332357ae1c7013f06bb495fcecd0d
某M <mmnstar@hotmail.com>
2016-12-09 19:53:52 +0800

    20161209.2

--- TBaoAutoSubmit/TBaoAutoSubmit.user.js
+++ TBaoAutoSubmit/TBaoAutoSubmit.user.js
@@ -3,8 +3,10 @@
 // @description 淘宝自动提交订单
 // @namespace   tbas
 // @include     *buy.taobao.com*confirm_order.htm*
+// @include     *buy.taobao.com*buy_now.jhtml*
 // @include     *buy.tmall.com*confirm_order.htm*
-// @version     2016.12.09.1
+// @include     *buy.tmall.com*buy_now.jhtml*
+// @version     2016.12.09.2
 // @grant       none
 // @run-at      document-end
 // @downloadURL https://github.com/moummn/MMN/raw/master/TBaoAutoSubmit/TBaoAutoSubmit.user.js
@@ -12,7 +14,12 @@
 // ==/UserScript==
 
 var t = setTimeout("location.reload()", 1000);
-var SubmitArea = document.getElementById("submitOrder_1");
-var SubmitElem = SubmitArea.getElementsByTagName("a")[1];
-//window.alert(SubmitElem.innerHTML);
-SubmitElem.click();
+var AllA = document.getElementsByTagName("a");
+for (var A = 0; A < AllA.length; A++) {
+    var SubmitElem = AllA[A];
+    if (SubmitElem.innerHTML == "提交订单") {
+        SubmitElem.click();
+    };
+};
+
+
