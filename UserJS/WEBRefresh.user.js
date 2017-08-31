2036e0eba655899af524fe053e5c01d985192a07
某M <mmn@1.1>
2017-08-31 23:21:19 +0800

    20170831

--- UserJS/WEBRefresh.user.js
+++ UserJS/WEBRefresh.user.js
@@ -2,8 +2,11 @@
 // @name        WEBRefresh
 // @namespace   WEBRefresh
 // @description 自定义刷新网页
-// @version     2017.08.31
+// @version     2017.08.31.1
+// @include     *www.natbbs.com*
 // @grant       none
+// @downloadURL https://github.com/moummn/MMN/raw/master/UserJS/WEBRefresh.user.js
+// @updateURL   https://github.com/moummn/MMN/raw/master/UserJS/WEBRefresh.user.js
 // ==/UserScript==
 
 //检测网页的URL，根据URL判断模式
