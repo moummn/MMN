deba34fb675b57d5671328a4fd9beced7682dc2c
moummn <mmnstar@sina.com>
2017-08-31 23:24:23 +0800

    Update WEBRefresh.user.js

--- UserJS/WEBRefresh.user.js
+++ UserJS/WEBRefresh.user.js
@@ -2,7 +2,7 @@
 // @name        WEBRefresh
 // @namespace   WEBRefresh
 // @description 自定义刷新网页
-// @version     2017.08.31.1
+// @version     2017.08.31.2
 // @include     *www.natbbs.com*
 // @grant       none
 // @downloadURL https://github.com/moummn/MMN/raw/master/UserJS/WEBRefresh.user.js
@@ -12,6 +12,6 @@
 //检测网页的URL，根据URL判断模式
 var thisURL = document.URL;
 if (thisURL.indexOf("www.natbbs.com") > 0) {
-    var t = setTimeout("location.reload()", 5000);
+    var t = setTimeout("location.reload()", 60000);
     //window.close();
 };
