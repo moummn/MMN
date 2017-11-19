30ec180338960f07b5e709a986fb73676d29baa1
某M <mmn@1.1>
2017-11-19 21:23:17 +0800

    20171119

--- HTOAPDF/HTOAPDF.user.js
+++ HTOAPDF/HTOAPDF.user.js
@@ -3,20 +3,35 @@
 // @description OA系统PDF页面修正
 // @namespace   HTOAPDF
 // @include     *223.100.151.35:8989/OAapp/HtTrace*
-// @version     2017.06.18.4
+// @version     2017.11.19.0
 // @grant       none
 // @run-at      document-end
 // @downloadURL https://github.com/moummn/MMN/raw/master/HTOAPDF/HTOAPDF.user.js
 // @updateURL   https://github.com/moummn/MMN/raw/master/HTOAPDF/HTOAPDF.user.js
 // ==/UserScript==
 
-//获得PDF地址
+//获得基本信息
+var pdfTable1 = document.getElementById("pdfTable");
 var wb1 = document.getElementById("wb");
-var src1 = wb1.src;
-//window.alert(src1);
+var src1;
+
+//获得PDF地址
+var AllScripts = document.getElementsByTagName("script");
+var SP1;
+for (var A = 0; A < AllScripts.length; A++) {
+        var SubmitElem = AllScripts[A].innerHTML;
+  			SP1=SubmitElem.indexOf("wb.src=hostAdd + \"") ;
+        if (SP1 >= 0) {
+          	src1=SubmitElem.substring(SP1+17,SubmitElem.length);
+          	SP2=src1.indexOf(".pdf") ;
+          	src1=src1.substring(1,SP2+4);
+          	break;
+        };
+    };
+
+window.alert(src1);
 
 //添加embed元素以显示pdf
-var pdfTable1 = document.getElementById("pdfTable");
 var newEm = document.createElement("embed");
 newEm.id = "pdfTable_embed";
 pdfTable1.appendChild(newEm);
