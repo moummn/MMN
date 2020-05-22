994a7fa3764afeabc094fc871a01d10dfdfaa69c
moummn <mmnstar@sina.com>
2020-05-22 11:18:29 +0800

    20200522

---
+++ jsWebFixEx/WebFixEx.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name     WebFixEx
+// @description 修正网页问题
+// @namespace   WebFixEx
+// @include     *www.moon-soft.com/book/*
+// @version  2020.05.22.0
+// @grant    none
+// @run-at      document-end
+// ==/UserScript==
+
+//判断网页网址
+var thisURL = document.URL;
+if (thisURL.indexOf("www.moon-soft.com/book/") > 0) {
+    var newStyle = "table[width=\"673\"]{margin:auto;}";
+    addStyle(newStyle);
+};
+
+
+//应用样式
+function addStyle(newStyle) {
+    var styleElement = document.createElement('style');
+    styleElement.type = 'text/css';
+    //styleElement.id = 'styles_js';
+    document.getElementsByTagName('head')[0].appendChild(styleElement);
+    styleElement.appendChild(document.createTextNode(newStyle));
+};
