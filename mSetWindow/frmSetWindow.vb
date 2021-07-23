574c568d4636448f4afd614fdbedf1a5f4684ca1
moummn <mmnstar@sina.com>
2021-07-23 08:47:18 +0800

    20210723

--- mSetWindow/frmSetWindow.vb
+++ mSetWindow/frmSetWindow.vb
@@ -10,6 +10,7 @@
         'frmSettings.Show()
         'frmSettings.sbFormRefresh()
         Call btnRefresh_Click(sender, e)
+
     End Sub
 
     'Private Sub frmWindowList_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
@@ -48,16 +49,20 @@
             End If
             ScanHandle = GetWindow(ScanHandle, GW_HWNDNEXT)
         Loop
+        'sbAllDisable()
 
     End Sub
     Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
         If ListView1.SelectedItems.Count = 0 Then
+            'sbAllDisable()
             Exit Sub
+        Else
+            'sbAllEnable()
         End If
         CheckBoxRefreshStatus = True
         CurrenthWnd = CInt(ListView1.SelectedItems(0).SubItems(1).Text)
         'frmSettings.sbWLRefresh(hWnd)
-        lblHandle.Text = CStr(CurrenthWnd)
+        lblHandle.Text = CStr(CurrenthWnd) & "    0x" & Hex(CurrenthWnd)
         Dim intWS As Integer = GetWindowLong(CurrenthWnd, GWL_STYLE)
         lblWS.Text = CStr(intWS) & "    0x" & Hex(intWS)
         Dim intWSEX As Integer = GetWindowLong(CurrenthWnd, GWL_EXSTYLE)
@@ -345,6 +350,7 @@
             Case Else
                 MsgBox(sender.ToString)
         End Select
+        ListView1_SelectedIndexChanged(sender, e)
     End Sub
 
     Private Sub cbWS_EX_CheckedChanged(sender As Object, e As EventArgs) Handles cbWS_EX_ACCEPTFILES.CheckedChanged,
@@ -570,5 +576,124 @@
             Case Else
                 MsgBox(sender.ToString)
         End Select
+        ListView1_SelectedIndexChanged(sender, e)
+    End Sub
+    Private Sub sbAllDisable()
+        cbWS_BORDER.Enabled = False
+        cbWS_CAPTION.Enabled = False
+        cbWS_CHILD.Enabled = False
+        cbWS_CHILDWINDOW.Enabled = False
+        cbWS_CLIPCHILDREN.Enabled = False
+        cbWS_CLIPSIBLINGS.Enabled = False
+        cbWS_DISABLED.Enabled = False
+        cbWS_DLGFRAME.Enabled = False
+        cbWS_GROUP.Enabled = False
+        cbWS_HSCROLL.Enabled = False
+        cbWS_ICONIC.Enabled = False
+        cbWS_MAXIMIZE.Enabled = False
+        cbWS_MAXIMIZEBOX.Enabled = False
+        cbWS_MINIMIZE.Enabled = False
+        cbWS_MINIMIZEBOX.Enabled = False
+        'cbWS_OVERLAPPED.Enabled = False
+        cbWS_OVERLAPPEDWINDOW.Enabled = False
+        cbWS_POPUP.Enabled = False
+        cbWS_POPUPWINDOW.Enabled = False
+        cbWS_SIZEBOX.Enabled = False
+        cbWS_SYSMENU.Enabled = False
+        cbWS_TABSTOP.Enabled = False
+        cbWS_THICKFRAME.Enabled = False
+        cbWS_TILED.Enabled = False
+        cbWS_TILEDWINDOW.Enabled = False
+        cbWS_VISIBLE.Enabled = False
+        cbWS_VSCROLL.Enabled = False
+
+        cbWS_EX_ACCEPTFILES.Enabled = False
+        cbWS_EX_APPWINDOW.Enabled = False
+        cbWS_EX_CLIENTEDGE.Enabled = False
+        cbWS_EX_COMPOSITED.Enabled = False
+        cbWS_EX_CONTEXTHELP.Enabled = False
+        cbWS_EX_CONTROLPARENT.Enabled = False
+        cbWS_EX_DLGMODALFRAME.Enabled = False
+        cbWS_EX_LAYERED.Enabled = False
+        cbWS_EX_LAYOUTRTL.Enabled = False
+        'cbWS_EX_LEFT.Enabled = False
+        cbWS_EX_LEFTSCROLLBAR.Enabled = False
+        cbWS_EX_LTRREADING.Enabled = False
+        cbWS_EX_MDICHILD.Enabled = False
+        cbWS_EX_NOACTIVATE.Enabled = False
+        cbWS_EX_NOINHERITLAYOUT.Enabled = False
+        cbWS_EX_NOPARENTNOTIFY.Enabled = False
+        cbWS_EX_NOREDIRECTIONBITMAP.Enabled = False
+        cbWS_EX_OVERLAPPEDWINDOW.Enabled = False
+        cbWS_EX_PALETTEWINDOW.Enabled = False
+        cbWS_EX_RIGHT.Enabled = False
+        cbWS_EX_RIGHTSCROLLBAR.Enabled = False
+        cbWS_EX_RTLREADING.Enabled = False
+        cbWS_EX_STATICEDGE.Enabled = False
+        cbWS_EX_TOOLWINDOW.Enabled = False
+        cbWS_EX_TOPMOST.Enabled = False
+        cbWS_EX_TRANSPARENT.Enabled = False
+        cbWS_EX_WINDOWEDGE.Enabled = False
+
+        lblHandle.Text = "0"
+        lblWS.Text = "0"
+        lblWSEX.Text = "0"
+    End Sub
+    Private Sub sbAllEnable()
+        cbWS_BORDER.Enabled = True
+        cbWS_CAPTION.Enabled = True
+        cbWS_CHILD.Enabled = True
+        cbWS_CHILDWINDOW.Enabled = True
+        cbWS_CLIPCHILDREN.Enabled = True
+        cbWS_CLIPSIBLINGS.Enabled = True
+        cbWS_DISABLED.Enabled = True
+        cbWS_DLGFRAME.Enabled = True
+        cbWS_GROUP.Enabled = True
+        cbWS_HSCROLL.Enabled = True
+        cbWS_ICONIC.Enabled = True
+        cbWS_MAXIMIZE.Enabled = True
+        cbWS_MAXIMIZEBOX.Enabled = True
+        cbWS_MINIMIZE.Enabled = True
+        cbWS_MINIMIZEBOX.Enabled = True
+        'cbWS_OVERLAPPED.Enabled =True
+        cbWS_OVERLAPPEDWINDOW.Enabled = True
+        cbWS_POPUP.Enabled = True
+        cbWS_POPUPWINDOW.Enabled = True
+        cbWS_SIZEBOX.Enabled = True
+        cbWS_SYSMENU.Enabled = True
+        cbWS_TABSTOP.Enabled = True
+        cbWS_THICKFRAME.Enabled = True
+        cbWS_TILED.Enabled = True
+        cbWS_TILEDWINDOW.Enabled = True
+        cbWS_VISIBLE.Enabled = True
+        cbWS_VSCROLL.Enabled = True
+
+        cbWS_EX_ACCEPTFILES.Enabled = True
+        cbWS_EX_APPWINDOW.Enabled = True
+        cbWS_EX_CLIENTEDGE.Enabled = True
+        cbWS_EX_COMPOSITED.Enabled = True
+        cbWS_EX_CONTEXTHELP.Enabled = True
+        cbWS_EX_CONTROLPARENT.Enabled = True
+        cbWS_EX_DLGMODALFRAME.Enabled = True
+        cbWS_EX_LAYERED.Enabled = True
+        cbWS_EX_LAYOUTRTL.Enabled = True
+        'cbWS_EX_LEFT.Enabled =True
+        cbWS_EX_LEFTSCROLLBAR.Enabled = True
+        cbWS_EX_LTRREADING.Enabled = True
+        cbWS_EX_MDICHILD.Enabled = True
+        cbWS_EX_NOACTIVATE.Enabled = True
+        cbWS_EX_NOINHERITLAYOUT.Enabled = True
+        cbWS_EX_NOPARENTNOTIFY.Enabled = True
+        cbWS_EX_NOREDIRECTIONBITMAP.Enabled = True
+        cbWS_EX_OVERLAPPEDWINDOW.Enabled = True
+        cbWS_EX_PALETTEWINDOW.Enabled = True
+        cbWS_EX_RIGHT.Enabled = True
+        cbWS_EX_RIGHTSCROLLBAR.Enabled = True
+        cbWS_EX_RTLREADING.Enabled = True
+        cbWS_EX_STATICEDGE.Enabled = True
+        cbWS_EX_TOOLWINDOW.Enabled = True
+        cbWS_EX_TOPMOST.Enabled = True
+        cbWS_EX_TRANSPARENT.Enabled = True
+        cbWS_EX_WINDOWEDGE.Enabled = True
     End Sub
 End Class
