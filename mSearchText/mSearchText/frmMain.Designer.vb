f02ae86b8853bc69645ec88e828de1f1095834f7
moummn <mmnstar@sina.com>
2023-02-27 15:51:54 +0800

    20230227

---
+++ mSearchText/mSearchText/frmMain.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 14)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SearchKW:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(88, 14)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(479, 23)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 14)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "FdFile:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(88, 46)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(479, 23)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 14)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "OptFile:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox3.Location = New System.Drawing.Point(88, 76)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(479, 23)
+        Me.TextBox3.TabIndex = 5
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(15, 114)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(117, 30)
+        Me.btnStart.TabIndex = 6
+        Me.btnStart.Text = "&Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 161)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(552, 23)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(582, 197)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMain"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "mSearchText"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents btnStart As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ mSearchText/mSearchText/frmMain.vb
@@ -0,0 +1,91 @@
+﻿Public Class frmMain
+    Private OutputText As String
+    Private Function fnGetFolder(ByVal FilePath As String) As String
+        Dim I As Integer = 0
+        Dim S As String = Replace(FilePath, "/", "\")
+        Dim TS As String = ""
+        Dim LastP As Integer = 0
+        For I = 1 To Len(S)
+            TS = Mid(S, I, 1)
+            If TS = "\" Then LastP = I
+        Next
+        If LastP > 0 Then
+            I = InStr(LastP, S, ".", vbBinaryCompare)
+        Else
+            I = 0
+        End If
+        If I > 0 Then
+            S = Mid(S, 1, LastP)
+        End If
+        If Strings.Right(S, 1) <> "\" Then
+            S &= "\"
+        End If
+        Return S
+    End Function
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        TextBox3.Text = fnGetFolder(sender.Text) & "Output.txt"
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If TextBox3.Text = "" Then TextBox3.Text = "Output.txt"
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+        TextBox3.Enabled = False
+        btnStart.Enabled = False
+
+        Dim SearchKeyWord As String = TextBox1.Text
+        Dim SearchFolder As String = TextBox2.Text
+        Dim WorkFolder As String = ""
+        Dim OutputFile As String = TextBox3.Text
+        Dim AllFiles As Integer = 0
+        Dim FileBuffer As String = ""
+        OutputText = ""
+
+        Dim I As Integer = 0
+
+        WorkFolder = fnGetFolder(SearchFolder)
+        If Mid(WorkFolder, 1, Len(WorkFolder) - 1) = SearchFolder Then
+            SearchFolder = WorkFolder
+        End If
+
+        Dim FileName As String = FileSystem.Dir(SearchFolder)
+        Do
+            If FileName = "" Then Exit Do
+            AllFiles += 1
+            FileName = FileSystem.Dir()
+        Loop
+
+        If AllFiles >= 1 Then
+
+            ProgressBar1.Maximum = AllFiles
+            ProgressBar1.Minimum = 0
+
+
+
+            FileName = FileSystem.Dir(SearchFolder)
+            I = 0
+            Do
+                If FileName = "" Then Exit Do
+
+                FileBuffer = My.Computer.FileSystem.ReadAllText(WorkFolder & FileName, System.Text.Encoding.Default)
+                Debug.Print(FileBuffer)
+                If InStr(FileBuffer, SearchKeyWord, vbBinaryCompare) > 0 Then
+                    OutputText &= WorkFolder & FileName & vbCrLf
+                End If
+                I += 1
+                FileName = FileSystem.Dir()
+                ProgressBar1.Value = I
+                My.Application.DoEvents()
+            Loop
+            My.Computer.FileSystem.WriteAllText(OutputFile, OutputText, False)
+
+
+        End If
+
+        TextBox1.Enabled = True
+        TextBox2.Enabled = True
+        TextBox3.Enabled = True
+        btnStart.Enabled = True
+        ProgressBar1.Value = 0
+    End Sub
+End Class
