e30766346f1cedac9545e699a9c2379d4ced9e1e
MMN <mmnstar@sina.com>
2023-09-17 21:51:53 +0800

    20230917

--- mVolTune/mVolTune/mVolTune.vb
+++ mVolTune/mVolTune/mVolTune.vb
@@ -1,4 +1,6 @@
-﻿Public Class mVolTune
+﻿Imports System.ComponentModel.Design.Serialization
+
+Public Class mVolTune
     '音量调节相关API
     Private Declare Function SendMessage Lib "user32" Alias "SendMessageA" _
         (ByVal hwnd As Integer, ByVal wMsg As Integer, ByVal wParam As Integer, lParam As Integer) As Long
@@ -7,16 +9,19 @@
     Private Const APPCOMMAND_VOLUME_DOWN As Integer = &H90000
     Private Const WM_APPCOMMAND As Integer = &H319
     '全局热键相关API
-    Public Const WM_HOTKEY = &H312
-    Public Const MOD_ALT = &H1
-    Public Const MOD_CONTROL = &H2
-    Public Const MOD_SHIFT = &H4
-    Public Const GWL_WNDPROC = (-4)
-    Public Declare Auto Function RegisterHotKey Lib "user32.dll" Alias "RegisterHotKey" _
+    Private Const WM_HOTKEY = &H312
+    Private Const MOD_ALT = &H1
+    Private Const MOD_CONTROL = &H2
+    Private Const MOD_SHIFT = &H4
+    Private Const GWL_WNDPROC = (-4)
+    Private Declare Auto Function RegisterHotKey Lib "user32.dll" Alias "RegisterHotKey" _
         (ByVal hwnd As IntPtr, ByVal id As Integer, ByVal fsModifiers As Integer,
          ByVal vk As Integer) As Boolean
-    Public Declare Auto Function UnRegisterHotKey Lib "user32.dll" Alias "UnregisterHotKey" _
+    Private Declare Auto Function UnRegisterHotKey Lib "user32.dll" Alias "UnregisterHotKey" _
         (ByVal hwnd As IntPtr, ByVal id As Integer) As Boolean
+    ''调节显示器亮度API（测试）
+    'Private Declare Auto Function SetMonitorBrightness Lib "dxva2.dll" Alias "SetMonitorBrightness" _
+    '    (ByVal hMonitor As Integer, ByVal dwNewBrightness As UInteger)
 
     Private Sub mVolTune_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -24,6 +29,7 @@
         RegisterHotKey(Handle, 0, MOD_CONTROL + MOD_ALT, Keys.Right) '第一个热键
         'RegisterHotKey(Handle, 1, Nothing, Keys.F4) '第二个热键
         RegisterHotKey(Handle, 1, MOD_CONTROL + MOD_ALT, Keys.Left) '第二个热键
+        RegisterHotKey(Handle, 1, MOD_CONTROL + MOD_ALT, Keys.Up)
     End Sub
     Private Sub mVolTune_FormClosed(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles MyBase.FormClosed
         '注销全局热键
@@ -40,6 +46,9 @@
                     SendMessage(Me.Handle, WM_APPCOMMAND, Me.Handle, APPCOMMAND_VOLUME_UP)
                 Case &H250003
                     SendMessage(Me.Handle, WM_APPCOMMAND, Me.Handle, APPCOMMAND_VOLUME_DOWN)
+                Case &H260003
+                    Dim TS As New Management.Instrumentation.ManagementKeyAttribute
+                    Debug.Print(TS.ToString)
             End Select
         End If
         MyBase.WndProc(m)
