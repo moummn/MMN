a79c78c8987ea0e3513ccb67f6b0ae9763663417
MMN <mmnstar@hotmail.com>
2025-04-03 15:38:09 +0800

    20250403

---
+++ KeyPressPrintScreen/frmKeyPressPrintScreen.Designer.vb
@@ -0,0 +1,42 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmKeyPressPrintScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'frmKeyPressPrintScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1, 1)
+        Me.ControlBox = False
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Location = New System.Drawing.Point(-1, -1)
+        Me.Name = "frmKeyPressPrintScreen"
+        Me.Opacity = 0R
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "frmKeyPressPrintScreen"
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
---
+++ KeyPressPrintScreen/frmKeyPressPrintScreen.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class frmKeyPressPrintScreen
+    ' 声明 keybd_event API
+    <DllImport("user32.dll", CharSet:=CharSet.Auto, SetLastError:=True)>
+    public Shared Sub keybd_event(bVk As Byte, bScan As Byte, dwFlags As UInteger, dwExtraInfo As UIntPtr)
+    End Sub
+    Private Const VK_SNAPSHOT As Byte = &H2C ' PrintScreen 键
+
+    Private Sub frmKeyPressPrintScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 按下 PrintScreen 键
+        keybd_event(VK_SNAPSHOT, 0, 0, UIntPtr.Zero)
+        ' 释放 PrintScreen 键（KEYEVENTF_KEYUP 标志）
+        keybd_event(VK_SNAPSHOT, 0, &H2, UIntPtr.Zero)
+        Me.Close()
+    End Sub
+
+End Class
